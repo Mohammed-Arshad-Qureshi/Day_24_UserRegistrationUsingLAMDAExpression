d55399871af012e4f28d842e87876aac0aabb088
Arshad <mohammedarshad183@gmail.com>
2022-06-04 06:45:23 +0530

    Displaying: welcome massgae for user Registration program

---
+++ UserRegistrationLamda/UserRegistrationLamda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationLamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to User Registration Validation");
+        }
+    }
+}

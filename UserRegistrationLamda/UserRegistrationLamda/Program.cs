25c86139dcbc34e9910e5b6488eea3aba3c3ccc1
Arshad <mohammedarshad183@gmail.com>
2022-06-04 07:19:58 +0530

    Added: As a User need to enter a valid First Name

--- UserRegistrationLamda/UserRegistrationLamda/Program.cs
+++ UserRegistrationLamda/UserRegistrationLamda/Program.cs
@@ -6,7 +6,16 @@ namespace UserRegistrationLamda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to User Registration Validation");
+            SetValidation validation = new SetValidation(); 
+            Console.WriteLine("\nWelcome to C# User's Information validation using Regular Expression");
+            //User First name validation            
+            Firstname:
+            Console.WriteLine("\nNote : Your first name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.Write("\n\nEnter your first name :   ");
+            string fName = Console.ReadLine(); 
+            bool checkFirstName = validation.FirstNameValidation(fName);  
+            if (!checkFirstName)//If not valid then ask user to enter first name again
+                goto Firstname;
         }
     }
 }
---
+++ UserRegistrationLamda/UserRegistrationLamda/SetValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationLamda
+{
+    class SetValidation
+    {
+        public bool FirstNameValidation(string fName) 
+        {
+
+            // Regex for first name validation
+            string pattern = "^[A-Z][a-z]{2,}$"; 
+            //If name entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(fName, pattern))
+            {
+                Console.WriteLine("\nYour first name {0} is valid",fName);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of first name");
+                return false;
+            }
+        }
+    }
+}

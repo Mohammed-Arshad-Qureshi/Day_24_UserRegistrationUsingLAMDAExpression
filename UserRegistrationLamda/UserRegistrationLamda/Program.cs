0bba8cce784c589164af14c70af475cbb20c6400
Arshad <mohammedarshad183@gmail.com>
2022-06-04 07:27:09 +0530

    As a User need to enter a valid Last Name

--- UserRegistrationLamda/UserRegistrationLamda/Program.cs
+++ UserRegistrationLamda/UserRegistrationLamda/Program.cs
@@ -16,6 +16,16 @@ namespace UserRegistrationLamda
             bool checkFirstName = validation.FirstNameValidation(fName);  
             if (!checkFirstName)//If not valid then ask user to enter first name again
                 goto Firstname;
+
+
+            // User Last name validation
+            Lastname:
+            Console.WriteLine("\nNote : Your Last name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your last name : ");
+            string lName = Console.ReadLine();
+            bool checkLastName = validation.LastNameValidation(lName);
+            if (!checkLastName)
+                goto Lastname;
         }
     }
 }
--- UserRegistrationLamda/UserRegistrationLamda/SetValidation.cs
+++ UserRegistrationLamda/UserRegistrationLamda/SetValidation.cs
@@ -7,21 +7,37 @@ namespace UserRegistrationLamda
 {
     class SetValidation
     {
-        public bool FirstNameValidation(string fName) 
+        public bool FirstNameValidation(string fName)
         {
 
             // Regex for first name validation
-            string pattern = "^[A-Z][a-z]{2,}$"; 
-            //If name entered by user is match with regex then it is valid otherwise not
+            string pattern = "^[A-Z][a-z]{2,}$";
             if (Regex.IsMatch(fName, pattern))
             {
-                Console.WriteLine("\nYour first name {0} is valid",fName);
+                Console.WriteLine("\nYour first name {0} is valid", fName);
                 return true;
             }
             else
             {
                 Console.WriteLine($"\nYour first name \"{fName}\" is not valid");
                 Console.WriteLine("\nPlease follow naming convention of first name");
+                return false;
+            }
+        }
+
+
+        public bool LastNameValidation(string lName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Regex for last name validation
+            if (Regex.IsMatch(lName, pattern))
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour last name \"{lName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of last name");
                 return false;
             }
         }
